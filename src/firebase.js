5c75efa6bddb385ade2924c0f803cd1d3f897657
malaikaikartel <50528583+malaikaikartel@users.noreply.github.com>
2023-06-05 08:30:41 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/storage";
 
 if (!firebase.apps.length) {
    firebase.initializeApp({
-      apiKey: AIzaSyBy1fxtGHrXVFlHHYUVafny5SHV8rVCs6c,
-  authDomain: redframe-77d5a.firebaseapp.com,
-  projectId: redframe-77d5a,
-  storageBucket: redframe-77d5a.appspot.com,
-  messagingSenderId: 806740627891,
-  appId: 1:806740627891:web:7025d1386396f9cdb3fbc6,
-  measurementId: G-HSL5EMRL9W,
+apiKey: process.env.REACT_APP_FIREBASE_PUBLIC_API_KEY,
+      authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+      databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+      projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+      storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.REACT_APP_FIREBASE_APP_ID,
    });
 }
 
